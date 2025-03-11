248bafa60e173e19c5e4b7e091e5f84670e19095
Rod Johnson <RJohnson@techvtal.com>
2025-03-10 22:51:25 -0600

    reorganize folders

---
+++ demo/AgencyPro/AgencyPro.Shared/Interfaces/IPerson.cs
@@ -0,0 +1,17 @@
+﻿namespace AgencyPro.Shared.Interfaces
+{
+    public interface IPerson
+    {
+        Guid Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string ImageUrl { get; set; }
+        string City { get; set; }
+        string Iso2 { get; set; }
+        string ProvinceState { get; set; }
+        
+        PersonStatus Status { get; set; }
+        DateTimeOffset Created { get; set; }
+        DateTimeOffset Updated { get; set; }
+    }
+}
